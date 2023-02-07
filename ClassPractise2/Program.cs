33e9752d85365a2a7825bd2e92e25b41b394831c
Serhii <mywaifuqueen@gmail.com>
2023-02-07 09:39:00 +0000

    Add project files.

---
+++ ClassPractise2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Vector a = new Vector(1, 2, 3);
+           Vector b = new Vector(2, 3, 4);
+           Vector c = new Vector(0,0,0);
+
+           Console.WriteLine("Vector a:" + a.ToString() );
+           Console.WriteLine("Vector b:" + b.ToString() );
+           Console.WriteLine("Enter vector c: \n");
+           c.Input();
+           Console.WriteLine("Vector c:" + c.ToString() + "");
+           Console.WriteLine("Vector a lenght: " + a.GetVectorLength());
+           Console.WriteLine("Vector b lenght: " + b.GetVectorLength());
+           Console.WriteLine("Vector c lenght: " + c.GetVectorLength());
+           Console.WriteLine("The vector a has been scaled up by the scalar a & b:");
+           a.IncreaseByScalar(b);
+           Console.WriteLine(a.ToString());
+           Console.WriteLine("The vector b has been scaled down by the scalar b & c:");
+           b.ReduceByScalar(c);
+           Console.WriteLine(b.ToString());
+
+           Vector d = Vector.AddVectors(a, b);
+           Console.WriteLine("The sum of the vector a and b: " + d.ToString());
+
+           d = Vector.SubstractVectors(a, b);
+           Console.WriteLine("The difference of the vector a and b: " + d.ToString());
+           
+           d = Vector.MultiplyVectors(a, c);
+           Console.WriteLine("The vector a and c have been multiplied: " + d.ToString());
+
+           if(Vector.IsEqual(a,b) == true)
+            {
+                Console.WriteLine("a == b");
+            }
+            else
+            {
+                Console.WriteLine("a != b");
+            }
+        }
+    }
+}
---
+++ ClassPractise2/Vector.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractise2
+{
+    class Vector
+    {
+        
+        public double x{ get; set; }
+        public double y{ get; set; }
+        public double z { get; set; }
+        public Vector(double x)
+        {
+            this.x = x;
+        }
+        public Vector(double x, double y):this(x)
+        {
+            this.y = y;
+        }
+        public Vector(double x, double y, double z):this(x,y)
+        {
+            this.z = z;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter z: ");
+            z = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine("x - " + x);
+            Console.WriteLine("y - " + y);
+            Console.WriteLine("z - " + z);
+        }
+        public double GetVectorLength() => Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        public double GetScalar(Vector vector2) => x * vector2.x + y * vector2.y + z * vector2.z;
+
+        public void IncreaseByScalar(Vector vector2)
+        {
+            double scalar = GetScalar(vector2);
+            x -= scalar;
+            y -= scalar;    
+            z -= scalar;
+        }
+        public void ReduceByScalar(Vector vector2)
+        {
+            double scalar = GetScalar(vector2);
+            x += scalar;
+            y += scalar;
+            z += scalar;
+        }
+        
+        public static Vector AddVectors(Vector vector1, Vector vector2) => new Vector(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z);
+        public static Vector SubstractVectors(Vector vector1, Vector vector2) => new Vector(vector1.x - vector2.x, vector1.y - vector2.y, vector1.z - vector2.z);
+        public static Vector MultiplyVectors(Vector vector1, Vector vector2) => new Vector(vector1.x * vector2.x, vector1.y * vector2.y, vector1.z * vector2.z);
+        public static bool IsEqual(Vector vector1, Vector vector2)
+        {
+            if(vector1.x == vector2.x && vector1.y == vector1.y && vector1.z == vector2.z) return true;
+            return false;
+        }
+        public override string ToString()
+        {
+            return "\nx - " + x + "\ny - " + y + "\nz - " + z + "\n";
+        }
+
+    }
+}
